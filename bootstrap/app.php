134cbd5400d6caa212a130d5f8c4a7016a221ef9
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2025-12-01 16:10:21 +0700

    fix: error on middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
-        $middleware->alias('checkmid', \App\Http\Middleware\CheckMid::class);
+        $middleware->alias([
+            'checkmid', \App\Http\Middleware\CheckMid::class,
+        ]);
         // $middleware->web(append: [
         //     HandleAppearance::class,
         //     HandleInertiaRequests::class,
